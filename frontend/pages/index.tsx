e03ffe187366592863d7da66a021436e9b1e1921
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-07-03 06:11:01 +0800

    added animation to heading text

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,18 +1,44 @@
 // import Layout from "../components/layout"
 
+import { motion } from "framer-motion";
 import Card from "../components/card";
 
+const slideIn = {
+  hidden: {
+    x: '-100%',
+    y: '0%',
+  },
+  show: {
+    x: 0,
+    y: 0,
+    transition: {
+      type: 'spring',
+      delay: 0.4,
+      duration: 0.8,
+      ease: 'easeOut',
+    },
+  },
+};
+
+
 export default function IndexPage() {
+
   return (
     <div className="px-8 py-2 md:px-16 md:py-4">
       <section className="py-20">
         <div className="m-auto max-w-7xl space-y-12">
-          <div className="space-y-2">
+          <motion.div
+            className="space-y-2"
+            variants={slideIn}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+          >
             <h4 className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-teal-300 to-blue-600 md:text-5xl">Prompt Treasure</h4>
             <p className="text-lg text-white">
               100 Prompts for Midjourney
             </p>
-          </div>
+          </motion.div>
           <div className="grid grid-cols-1 gap-16 md:grid-cols-2 lg:grid-cols-3">
             <Card />
             <Card />
---
+++ frontend/utils/motion.ts
@@ -0,0 +1,161 @@
+const navVariants = {
+    hidden: {
+        opacity: 0,
+        y: -50,
+        transition: {
+            type: 'spring',
+            stiffness: 300,
+            damping: 140,
+        },
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 80,
+            delay: 1,
+        },
+    },
+};
+
+const slideIn = (direction: string, type: any, delay: any, duration: any) => ({
+    hidden: {
+        x: direction === 'left' ? '-100%' : direction === 'right' ? '100%' : 0,
+        y: direction === 'up' ? '100%' : direction === 'down' ? '100%' : 0,
+    },
+    show: {
+        x: 0,
+        y: 0,
+        transition: {
+            type,
+            delay,
+            duration,
+            ease: 'easeOut',
+        },
+    },
+});
+
+const staggerContainer = (staggerChildren: any, delayChildren: any) => ({
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren,
+            delayChildren,
+        },
+    },
+});
+
+const textVariant = (delay: any) => ({
+    hidden: {
+        y: 50,
+        opacity: 0,
+    },
+    show: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: 'spring',
+            duration: 1.25,
+            delay,
+        },
+    },
+});
+
+const textContainer = {
+    hidden: {
+        opacity: 0,
+    },
+    show: (i = 1) => ({
+        opacity: 1,
+        transition: { staggerChildren: 0.1, delayChildren: i * 0.1 },
+    }),
+};
+
+const textVariant2 = {
+    hidden: {
+        opacity: 0,
+        y: 20,
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'tween',
+            ease: 'easeIn',
+        },
+    },
+};
+
+const fadeIn = (direction: string, type: any, delay: any, duration: any) => ({
+    hidden: {
+        x: direction === 'left' ? 100 : direction === 'right' ? -100 : 0,
+        y: direction === 'up' ? 100 : direction === 'down' ? -100 : 0,
+        opacity: 0,
+    },
+    show: {
+        x: 0,
+        y: 0,
+        opacity: 1,
+        transition: {
+            type,
+            delay,
+            duration,
+            ease: 'easeOut',
+        },
+    },
+});
+
+const planetVariants = (direction: string) => ({
+    hidden: {
+        x: direction === 'left' ? '-100%' : '100%',
+        rotate: 120,
+    },
+    show: {
+        x: 0,
+        rotate: 0,
+        transition: {
+            type: 'spring',
+            duration: 1.8,
+            delay: 0.5,
+        },
+    },
+});
+
+const zoomIn = (delay: any, duration: any) => ({
+    hidden: {
+        scale: 0,
+        opacity: 0,
+    },
+    show: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            type: 'tween',
+            delay,
+            duration,
+            ease: 'easeOut',
+        },
+    },
+});
+
+const footerVariants = {
+    hidden: {
+        opacity: 0,
+        y: 50,
+        transition: {
+            type: 'spring',
+            stiffness: 300,
+            damping: 140,
+        },
+    },
+    show: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 80,
+            delay: 0.5,
+        },
+    },
+};
